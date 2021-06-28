a219f2456311af48365c6aa1ae709fe98799c02d
yangtianyu01 <yangtianyu01@corp.netease.com>
2021-06-28 20:58:07 +0800

    临时存储

--- src/bridge/Node.ts
+++ src/bridge/Node.ts
@@ -4,6 +4,7 @@ declare module 'uuid'
 
 enum NODEEVENT {
   init = '$nodeInit',
+  initBack = '$nodeInitBack',
   transmit = '$transmit'
 }
 interface NEvent extends Event {
@@ -14,11 +15,26 @@ export default class Node {
   private _id: string = ''
   private _bridgeMap: {[x: string]: Bridge} = {}
   private _nodeMap: {[x: string]: string} = {}
+  private delEvent(event: NEvent, bridegKey: string) {
+    return Object.assign({}, event, {
+      path: event.path ? `${bridegKey}__${event.path}` : bridegKey
+    })
+  }
   constructor({id, bridgeList}: {id: string, bridgeList: BridgeOptions[]}) {
     this._id = id;
     this.addBridge(bridgeList)
-    this.on(NODEEVENT.init, (data, event) => {
+    this.handle(NODEEVENT.initBack, (data, event) => {
+      if(data.id !=== this._id) {
 
+      }
+    })
+    this.on(NODEEVENT.init, (data, event) => {
+      console.log(NODEEVENT.init, data, event)
+      if(event.path) {
+        let pathList = event.path.split('__');
+        let bId = pathList.pop() || '';
+        this._bridgeMap[bId].invoke(NODEEVENT.initBack, {id, path: pathList.join('__')})
+      }
     })
   }
   addBridge(bridgeList: BridgeOptions[]) {
@@ -44,24 +60,36 @@ export default class Node {
       })
     }
   }
-  on(nodeId: string, event: string, fn: () => any): any
-  on(event: string, fn: () => any): any
+  on(nodeId: string, cannel: string, fn: (data: any, event: NEvent) => any): any
+  on(cannel: string, fn: (data: any, event: NEvent) => any): any
   on(a: string, b: any, c?: any): any {
     if(c) {
 
     } else {
       Object.keys(this._bridgeMap).forEach(key => {
         this._bridgeMap[key].on(a, (data: any, event: NEvent) => {
-          let e = Object.assign({}, event, {
-            path: event.path ? `${key}__${event.path}` : key
-          })
-          c(data, {event})
+          c(data, {...this.delEvent(event, key)})
         })
       })
     }
   }
   off() {}
   once() {}
-  handle() {}
+  on(nodeId: string, cannel: string, fn: () => any): any
+  on(cannel: string, fn: () => any): any
+  handle(a: string, b: any, c?: any): any {
+    if(c) {
+
+    } else {
+      Object.keys(this._bridgeMap).forEach(key => {
+        this._bridgeMap[key].handle(a, (data: any, event: NEvent) => {
+          let e = Object.assign({}, event, {
+            path: event.path ? `${key}__${event.path}` : key
+          })
+          return c(data, {...this.delEvent(event, key)})
+        })
+      })
+    }
+  }
   invoke() {}
 }
